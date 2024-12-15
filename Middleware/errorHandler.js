61eaf2d1cb21934703dec74e5ea6c467a52bfdde
david onwuli <dcazicruz@icloud.com>
2024-12-15 11:19:08 +0100

    fixed an issue with custom errors

--- Middleware/errorHandler.js
+++ Middleware/errorHandler.js
@@ -1,13 +1,14 @@
 const { logEvents } = require('./logEvents');
 
-const errorHandler = (err, res) => {
+const errorHandler =async (err, res) => {
     logEvents(`${err.name}: ${err.message}`, 'errLog.txt');
     console.error(err.stack)
     if (err.message) {
       //   // If the error has a message property, send it as the response
-      return res.status(err.status? err.status : 500).json({
-        statusCode: err.status? err.status : 500,
+      return res.status(err.statusCode? err.statusCode : 500).json({
+        statusCode: err.statusCode? err.statusCode : 500,
         message: err.message,
+        status:err.status
            });
        } else {
       //   // If there's no message property, send a generic error response
